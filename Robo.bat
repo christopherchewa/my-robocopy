07c6f257409c90d1f4e75faeb3e607a3f4c90164
christopchewa <chrischewa@gmail.com>
2020-09-26 23:26:58 +0300

    first commit

---
+++ Robo.bat
@@ -0,0 +1,7 @@
+
+NET USE \\KRONIKA\IPC$ /u:KRONIKA\chewa discombobulated
+robocopy \\KRONIKA\Games\Batman D:\batman /S /E /ETA /R:5 /W:5 /V /MIR /Z /MT:128 /LOG+:c:\temp\log.log /TEE
+NET USE \\KRONIKA\IPC$ /D
+
+
+
